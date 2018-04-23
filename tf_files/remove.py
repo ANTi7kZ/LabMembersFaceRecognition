7360c0504e243480b8fa2b67969e86d27bcaeba2
Mina Khan <minkhn01>
2018-04-23 12:32:57 -0400

    manually remove node

--- tf_files/remove.py
+++ tf_files/remove.py
@@ -7,7 +7,8 @@ graph.ParseFromString(open('rounded_graph.pb','rb').read())
 def display_nodes(nodes):
     for i, node in enumerate(nodes):
         print('%d %s %s' % (i, node.name, node.op))
-        [print(u'└─── %d ─ %s' % (i, n)) for i, n in enumerate(node.input)]
+        for j, n in enumerate(node.input):
+        	print('└─── %d ─ %s' % (j, n))
     
 display_nodes(graph.node)
 
